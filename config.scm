c1ba07d234284330b7ee1d9c5cf9f802f41cc08b
Peter Ahlstöm <peter.ahlstrom@gmail.com>
2021-02-02 14:32:48 +0100

    Update config.scm

--- config.scm
+++ config.scm
@@ -1,1 +1,12 @@
+;; Import nonfree linux module.
+(use-modules (nongnu packages linux)
+             (nongnu system linux-initrd))
 
+(operating-system
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware))
+  
+ 
+ 
+  )
